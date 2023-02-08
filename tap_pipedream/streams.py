559a811f7e6d0c7301a9e22b90989f1224c8c7fd
Edgar R. M <edgarrm358@gmail.com>
2023-02-08 16:53:24 -0600

    chore: Update dependencies (#105)
    * chore: Update dependencies
    
    * Drop `mypy` job

--- tap_pipedream/streams.py
+++ tap_pipedream/streams.py
@@ -17,12 +17,115 @@ class UserSources(PipedreamStream):
     schema = th.PropertiesList(
         th.Property("id", th.StringType),
         th.Property("component_id", th.StringType),
-        th.Property("configured_props", th.ObjectType()),
         th.Property("active", th.BooleanType),
         th.Property("created_at", th.IntegerType),
         th.Property("updated_at", th.IntegerType),
         th.Property("name", th.StringType),
         th.Property("name_slug", th.StringType),
+        th.Property("owner_id", th.StringType),
+        th.Property(
+            "configured_props",
+            th.ObjectType(
+                th.Property(
+                    "timer",
+                    th.ObjectType(
+                        th.Property("cron", th.StringType),
+                        th.Property("interval_seconds", th.IntegerType),
+                    ),
+                ),
+                th.Property(
+                    "url",
+                    th.StringType,
+                ),
+                th.Property(
+                    "httpRequest",
+                    th.ObjectType(
+                        th.Property(
+                            "auth",
+                            th.ObjectType(
+                                th.Property(
+                                    "type",
+                                    th.StringType,
+                                ),
+                                th.Property(
+                                    "username",
+                                    th.StringType,
+                                ),
+                                th.Property(
+                                    "password",
+                                    th.StringType,
+                                ),
+                                th.Property(
+                                    "token",
+                                    th.StringType,
+                                ),
+                            ),
+                        ),
+                        th.Property(
+                            "body",
+                            th.ObjectType(
+                                th.Property(
+                                    "contentType",
+                                    th.StringType,
+                                ),
+                                th.Property(
+                                    "fields",
+                                    th.ArrayType(th.StringType),
+                                ),
+                                th.Property(
+                                    "mode",
+                                    th.StringType,
+                                ),
+                                th.Property(
+                                    "type",
+                                    th.StringType,
+                                ),
+                            ),
+                        ),
+                        th.Property(
+                            "headers",
+                            th.ArrayType(th.StringType),
+                        ),
+                        th.Property(
+                            "method",
+                            th.StringType,
+                        ),
+                        th.Property("params", th.ArrayType(th.ObjectType())),
+                        th.Property(
+                            "tab",
+                            th.StringType,
+                        ),
+                        th.Property(
+                            "url",
+                            th.StringType,
+                        ),
+                    ),
+                ),
+                th.Property(
+                    "emitBodyOnly",
+                    th.BooleanType,
+                ),
+                th.Property(
+                    "emitAsArray",
+                    th.BooleanType,
+                ),
+            ),
+        ),
+    ).to_dict()
+
+
+class UserSubscriptions(PipedreamStream):
+    """Subscriptions stream."""
+
+    name = "user_subscriptions"
+    path = "/users/me/subscriptions"
+    primary_keys = ["id"]
+
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType),
+        th.Property("emitter_id", th.StringType),
+        th.Property("listener_id", th.StringType),
+        th.Property("event_name", th.StringType),
     ).to_dict()
 
 
@@ -46,19 +149,11 @@ class OrganizationSources(UserSources, _BaseOrganizationStream):
     path = "/orgs/{org_id}/sources/"
 
 
-class OrganizationSubscriptions(_BaseOrganizationStream):
+class OrganizationSubscriptions(UserSubscriptions, _BaseOrganizationStream):
     """Organization Subscriptions stream."""
 
     name = "organization_subscriptions"
     path = "/orgs/{org_id}/subscriptions/"
-    primary_keys = ["id"]
-
-    schema = th.PropertiesList(
-        th.Property("id", th.StringType),
-        th.Property("emitter_id", th.StringType),
-        th.Property("listener_id", th.StringType),
-        th.Property("event_name", th.StringType),
-    ).to_dict()
 
 
 class Webhooks(PipedreamStream):
@@ -77,3 +172,6 @@ class Webhooks(PipedreamStream):
         th.Property("created_at", th.IntegerType),
         th.Property("updated_at", th.IntegerType),
     ).to_dict()
+
+
+# class WorkflowEmits(
